485a2c956d7e04f7a097c1981d614455b0b4ac15
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-18 18:09:33 +0900

    <feat> add InputManager class and members

---
+++ Util/InputManager.cpp
@@ -0,0 +1,46 @@
+﻿#include "InputManager.h"
+#include "../Common.h"
+
+bool InputManager::m_currentKeyStates[256] = { false };
+bool InputManager::m_previousKeyStates[256] = { false };
+
+
+
+
+string InputManager::GetKeyName(int vkey)
+{
+	if ((vkey >= 'A' && vkey <= 'Z') || (vkey >= '0' && vkey <= '9'))
+	{
+		char keyChar = static_cast<char>(vkey);
+		return string(1, keyChar);
+	}
+	return "";
+}
+
+void InputManager::Update()
+{
+	memcpy(m_previousKeyStates, m_currentKeyStates, sizeof(m_currentKeyStates));
+
+	for (int vkey = 0; vkey < 256; ++vkey)
+	{
+		if (GetAsyncKeyState(vkey) & 0x8000)
+		{
+			m_currentKeyStates[vkey] = true;
+		}
+		else
+		{
+			m_currentKeyStates[vkey] = false;
+		}
+	}
+
+
+	//test
+	for (int i = 0; i < static_cast<int>(sizeof(m_currentKeyStates)); ++i)
+	{
+		if (m_currentKeyStates[i] == true && m_previousKeyStates[i] == false)
+		{
+			std::cout << InputManager::GetKeyName(i) << " 키가 눌렸습니다." << std::endl;
+		}
+	}
+
+}
---
+++ Util/InputManager.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "../Common.h"
+
+
+class InputManager
+{
+public:
+	InputManager() = delete;
+	~InputManager() = delete;
+
+private:
+	static bool m_currentKeyStates[256];
+	static bool m_previousKeyStates[256];
+
+private:
+	static string GetKeyName(int vkey);
+
+public:
+	static void Update();
+
+
+
+};
+

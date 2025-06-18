b63e85b37f879f3f7df56e9928a98e0695a8f17c
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-18 18:08:59 +0900

    <feat> add LootType, StatType structs and members

---
+++ Util/Type/LootType.h
@@ -0,0 +1,73 @@
+﻿#pragma once
+#include "../Common.h"
+
+struct Experience
+{
+	Experience() = default;
+	explicit Experience(int8 level) : m_level(level), m_currentExp(0) { }
+
+private:
+	int16 m_currentExp = 0;
+	int8 m_level = 1;
+
+	
+	int16 GetRequiredExpForNextLevel() const
+	{
+		//TODO : LevelData에서 불러오기
+		return 3 + (m_level - 1) * 5;
+	}
+
+public:
+	bool AddExperience(int16 amount)
+	{
+		m_currentExp += amount;
+		bool leveledUp = false;
+
+		while (m_currentExp >= GetRequiredExpForNextLevel())
+		{
+			m_currentExp -= GetRequiredExpForNextLevel();
+			m_level++;
+			leveledUp = true;
+		}
+		return leveledUp;
+	}
+};
+
+
+
+struct Gold
+{
+	Gold() = default;
+	explicit Gold(int16 initialAmount) : m_amount(initialAmount) { }
+
+private:
+	int16 m_amount = 10000;
+
+
+public:
+	void AddGold(int16 amount)
+	{
+		int32 total = static_cast<int32>(m_amount) + amount;
+
+		if (total > INT16_MAX) 
+		{ 
+			m_amount = INT16_MAX;
+		}
+		else 
+		{
+			m_amount = static_cast<int16>(total);
+		}
+	}
+
+	bool RemoveGold(int16 amount)
+	{
+		if (m_amount < amount)
+		{
+			return false;
+		}
+
+		m_amount -= amount;
+		
+		return true;
+	}
+};
---
+++ Util/Type/StatType.h
@@ -0,0 +1,91 @@
+﻿#pragma once
+#include "../Common.h"
+
+
+using namespace std;
+
+enum class EStatType : uint8
+{
+	HP,
+	ATTACK,
+	DEFENSE,
+	AGILITY,
+	MAX_COUNT
+};
+
+enum class EModifierType : uint8
+{
+	ADD,
+	SUBTRACT,
+	MUTIFLY
+};
+
+struct Modifier
+{
+	EStatType targetStatType;
+	EModifierType modifierType;
+	int8 value = 0;
+};
+
+struct ModifierContainer
+{
+	wstring id;
+	vector<Modifier> modifiers;
+};
+
+struct StatusValue
+{
+	int8 baseStatValue = 0;
+	int8 bonusStatValue = 0;
+	int16 finalStatValue = 0;
+	bool bIsChanged = true;
+};
+
+
+struct Status
+{
+	unordered_map<EStatType, StatusValue> m_statsMap;
+	unordered_map<wstring, ModifierContainer> m_modifierContainers;
+
+	int16	GetStatValue(EStatType statType)
+	{
+		unordered_map<EStatType, StatusValue>::iterator it = m_statsMap.find(statType);
+		
+		if (it == m_statsMap.end())
+		{
+			return 0;
+		}
+
+		if (it->second.bIsChanged == false)
+		{
+			return it->second.finalStatValue;
+		}
+
+		int16 tempCalculatedValue = CalculateStatusValue(statType);
+		it->second.finalStatValue = tempCalculatedValue;
+		it->second.bIsChanged = false;
+
+		return tempCalculatedValue;
+	}
+
+	void	AddModifierContainer(const ModifierContainer& container)
+	{
+
+	}
+
+	void	RemoveModifierContainer(const wstring& id)
+	{
+
+	}
+
+private:
+	int16	CalculateStatusValue(EStatType statType) const
+	{
+
+	}
+
+	void	SetChangeStatus(const ModifierContainer& container)
+	{
+
+	}
+};

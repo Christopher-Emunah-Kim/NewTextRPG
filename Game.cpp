cad403c341621d1ad6a647a7a34eba1d7a8d11ef
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-18 20:54:24 +0900

    <feat> make default game loop functions

--- Game.cpp
+++ Game.cpp
@@ -1,2 +1,46 @@
 ﻿#include "Game.h"
+#include "Util/Timer.h"
 
+Game::~Game() noexcept
+{
+	Release();
+}
+
+bool Game::Init()
+{
+
+}
+
+void Game::Run()
+{
+	Timer::Init();
+
+	while (true)
+	{
+
+
+		if (Timer::CanUpdate())
+		{
+			ProcessInput();
+			Update();
+			Render();
+		}
+	}
+
+}
+
+void Game::ProcessInput()
+{
+}
+
+void Game::Update()
+{
+}
+
+void Game::Render()
+{
+}
+
+void Game::Release()
+{
+}
--- Game.h
+++ Game.h
@@ -1,6 +1,19 @@
 ﻿#pragma once
+#include "Common.h"
+
 class Game
 {
+public:
+	virtual ~Game() noexcept;
+
+	bool Init();
+	void Run();
+
+private:
+	void ProcessInput();
+	void Update();
+	void Render();
+	void Release();
 
 };
 

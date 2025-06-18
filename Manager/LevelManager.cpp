9c8c6412782aa496aba80e17891483db3f0ef66c
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-18 21:53:44 +0900

    <feat> add LevelManager class

---
+++ Manager/LevelManager.cpp
@@ -0,0 +1,50 @@
+﻿#include "LevelManager.h"
+
+
+LevelManager::~LevelManager()
+{
+	Release();
+}
+
+
+void LevelManager::Init()
+{
+	
+}
+
+void LevelManager::Update()
+{
+	//m_currentLevel->Update();
+}
+
+void LevelManager::Render()
+{
+
+}
+
+void LevelManager::Release()
+{
+	m_currentLevel = nullptr;
+	m_nextLevel = nullptr;
+
+
+	for (unordered_map<string, BaseLevel*>::iterator it = m_levels.begin(); it != m_levels.end(); ++it)
+	{
+		delete it->second;
+	}
+
+}
+
+
+bool LevelManager::IsSetNextLevel() const
+{
+	return false;
+}
+
+void LevelManager::SetNextLevel(const string& name)
+{
+}
+
+void LevelManager::ChangeLevel()
+{
+}
---
+++ Manager/LevelManager.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "../Common.h"
+#include "../Util/Singleton.h"
+#include "../Core/IBehavior.h"
+
+class BaseLevel;
+
+class LevelManager : public Singleton<LevelManager>, public IBehavior
+{
+public:
+	LevelManager() = default;
+	virtual ~LevelManager();
+
+private:
+	BaseLevel* m_currentLevel = nullptr;
+	BaseLevel* m_nextLevel = nullptr;
+	unordered_map<string, BaseLevel*> m_levels;
+
+public:
+	virtual void Init() override;
+	virtual void Update() override;
+	virtual void Render() override;
+	virtual void Release() override;
+
+	bool IsSetNextLevel() const;
+	void SetNextLevel(const string& name);
+	void ChangeLevel();
+};
+

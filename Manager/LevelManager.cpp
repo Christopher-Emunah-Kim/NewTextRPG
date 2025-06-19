522afbe1b6da32cb494394fef680ff9d4b0661ae
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-19 15:16:25 +0900

    <feat> add default functions in LevelManager class

--- Manager/LevelManager.cpp
+++ Manager/LevelManager.cpp
@@ -1,4 +1,7 @@
 ﻿#include "LevelManager.h"
+#include "../Level/BaseLevel.h"
+#include "../Level/TestLevel.h"
+#include "../Util/OutputSystem.h"
 
 
 LevelManager::~LevelManager()
@@ -9,42 +12,66 @@ LevelManager::~LevelManager()
 
 void LevelManager::Init()
 {
-	
+	m_levels["Temp"] = new TestLevel("Temp");
+
+	m_currentLevel = m_levels["Temp"]; //TODO : 이후 Title로 변경
+	m_currentLevel->Init();
 }
 
 void LevelManager::Update()
 {
-	//m_currentLevel->Update();
+	m_currentLevel->Update();
 }
 
 void LevelManager::Render()
 {
-
+	m_currentLevel->Render();
 }
 
 void LevelManager::Release()
 {
-	m_currentLevel = nullptr;
-	m_nextLevel = nullptr;
+	if (m_currentLevel)
+	{
+		m_currentLevel->Release();
+		m_currentLevel = nullptr;
+	}
 
+	m_nextLevel = nullptr;
 
 	for (unordered_map<string, BaseLevel*>::iterator it = m_levels.begin(); it != m_levels.end(); ++it)
 	{
 		delete it->second;
 	}
 
+	m_levels.clear();
 }
 
 
 bool LevelManager::IsSetNextLevel() const
 {
-	return false;
+	return m_nextLevel != nullptr;
 }
 
 void LevelManager::SetNextLevel(const string& name)
 {
+	if (m_nextLevel == nullptr)
+	{
+		OutputSystem::PrintErrorMsg("설정된 NextLevel 이 존재합니다. 새로운 Level 을 설정할 수 없습니다.");
+		return;
+	}
+
+	unordered_map<string, BaseLevel*>::iterator it = m_levels.find(name);
+	if (it == m_levels.end())
+	{
+		OutputSystem::PrintErrorMsg(name + "라는 이름의 Level 이 존재하지 않습니다.");
+		return;
+	}
+
+	m_nextLevel = it->second;
+
 }
 
 void LevelManager::ChangeLevel()
 {
+
 }

268d32faa78ae3abb0303c6408e668e3a9ab24d9
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-19 21:34:07 +0900

    <feat> Add Title level class

---
+++ Level/TitleLevel.cpp
@@ -0,0 +1,14 @@
+﻿#include "TitleLevel.h"
+
+
+void TitleLevel::Init()
+{
+	BaseLevel::Init();
+}
+
+void TitleLevel::Update()
+{
+	BaseLevel::Update();
+
+
+}
---
+++ Level/TitleLevel.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "BaseLevel.h"
+
+class InputSystem;
+
+class TitleLevel : public BaseLevel
+{
+public:
+	using BaseLevel::BaseLevel;
+	virtual ~TitleLevel() = default;
+
+	virtual void Init() override;
+	virtual void Update() override;
+
+};
+

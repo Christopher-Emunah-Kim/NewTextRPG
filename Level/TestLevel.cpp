cbac19ca2668083ad370bad12f7a212f2c9aa44f
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-19 15:16:01 +0900

    <feat> make TestLevel for test

---
+++ Level/TestLevel.cpp
@@ -0,0 +1,14 @@
+﻿#include "TestLevel.h"
+
+
+void TestLevel::Init()
+{
+	BaseLevel::Init();
+}
+
+void TestLevel::Update()
+{
+	BaseLevel::Update();
+
+	
+}
---
+++ Level/TestLevel.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "BaseLevel.h"
+
+class TestLevel final : public BaseLevel
+{
+public:
+	TestLevel(const string& tag) : BaseLevel(tag) { }
+	virtual ~TestLevel() = default;
+
+	virtual void Init() override;
+	virtual void Update() override;
+};
+

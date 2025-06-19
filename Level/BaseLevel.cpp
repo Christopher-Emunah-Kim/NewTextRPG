a2902c27987c26c00e2ad201474bd8fe42d6f3a5
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-19 15:15:47 +0900

    <feat> make BaseLevel default functions

--- Level/BaseLevel.cpp
+++ Level/BaseLevel.cpp
@@ -1,1 +1,106 @@
 ﻿#include "BaseLevel.h"
+#include "../Object/BaseGameObject.h"
+
+BaseLevel::BaseLevel(const string& tag)
+	: m_levelTag(tag)
+{
+
+}
+
+BaseLevel::~BaseLevel()
+{
+	Release();
+}
+
+void BaseLevel::Init()
+{
+	for (size_t i = 0; i < m_gameObjects.size(); ++i)
+	{
+		BaseGameObject* obj = m_gameObjects[i];
+		if (obj)
+		{
+			obj->Init();
+		}
+	}
+}
+
+void BaseLevel::Update()
+{
+	for (size_t i = 0; i < m_gameObjects.size(); ++i)
+	{
+		BaseGameObject* obj = m_gameObjects[i];
+		if (obj)
+		{
+			obj->Update();
+		}
+	}
+}
+
+void BaseLevel::Render()
+{
+	for (size_t i = 0; i < m_gameObjects.size(); ++i)
+	{
+		BaseGameObject* obj = m_gameObjects[i];
+		if (obj)
+		{
+			obj->Render();
+		}
+	}
+}
+
+void BaseLevel::Release()
+{
+	for (size_t i = 0; i < m_gameObjects.size(); ++i)
+	{
+		BaseGameObject* obj = m_gameObjects[i];
+		if (obj)
+		{
+			obj->Release();
+
+			delete obj;
+			obj = nullptr;
+		}
+	}
+
+	m_gameObjects.clear();
+}
+
+void BaseLevel::AddObject(BaseGameObject* object)
+{
+	m_gameObjects.push_back(object);
+}
+
+void BaseLevel::RemoveObject(const string& tag)
+{
+	for (vector<BaseGameObject*>::iterator it = m_gameObjects.begin(); it != m_gameObjects.end();)
+	{
+		BaseGameObject* obj = *it;
+
+		if (obj && obj->GetTag() == tag) 
+		{
+			obj->Release();
+			delete obj;
+			obj = nullptr;
+
+			it = m_gameObjects.erase(it);
+		}
+		else
+		{
+			++it;
+		}
+	}
+}
+
+BaseGameObject* BaseLevel::FindObject(const string& tag)
+{
+	for (vector<BaseGameObject*>::iterator it = m_gameObjects.begin(); it != m_gameObjects.end(); ++it)
+	{
+		BaseGameObject* obj = *it;
+		if (obj && obj->GetTag() == tag)
+		{
+			return obj;
+		}
+	}
+
+	return nullptr;
+}
--- Level/BaseLevel.h
+++ Level/BaseLevel.h
@@ -2,9 +2,28 @@
 #include "../Common.h"
 #include "../Core/IBehavior.h"
 
+class BaseGameObject;
 
 class BaseLevel abstract : public IBehavior
 {
+public:
+	BaseLevel(const string& tag);
+	virtual ~BaseLevel();
 
+private:
+	string m_levelTag = " ";
+	vector<BaseGameObject*> m_gameObjects;
+
+public:
+	virtual void Init() override;
+	virtual void Update() override;
+	virtual void Render() override;
+	virtual void Release() override;
+
+	void AddObject(BaseGameObject* object);
+	void RemoveObject(const string& tag);
+	BaseGameObject* FindObject(const string& tag);
+
+	inline string GetTag() const noexcept { return m_levelTag; }
 };
 

dfb67de8b554fe3b036edfc79de55edcd1440e82
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-19 15:14:57 +0900

    <feat> rename BaseGameObject class and make default functions

---
+++ Object/BaseGameObject.cpp
@@ -0,0 +1,87 @@
+﻿#include "BaseGameObject.h"
+#include "../Level/BaseLevel.h"
+#include "../Component/BaseComponent.h"
+
+BaseGameObject::BaseGameObject(BaseLevel* level, const string& tag)
+	: m_level{level}, m_objectTag(tag)
+{
+	m_level->AddObject(this);
+}
+
+BaseGameObject::~BaseGameObject()
+{
+	Release();
+}
+
+void BaseGameObject::Init()
+{
+	for (size_t i = 0; i < m_components.size(); ++i)
+	{
+		BaseComponent* comp = m_components[i];
+		if (comp)
+		{
+			comp->Init();
+		}
+	}
+}
+
+void BaseGameObject::Update()
+{
+	for (size_t i = 0; i < m_components.size(); ++i)
+	{
+		BaseComponent* comp = m_components[i];
+		if (comp)
+		{
+			comp->Update();
+		}
+	}
+}
+
+
+void BaseGameObject::Render()
+{
+	for (size_t i = 0; i < m_components.size(); ++i)
+	{
+		BaseComponent* comp = m_components[i];
+		if (comp)
+		{
+			comp->Render();
+		}
+	}
+}
+
+void BaseGameObject::Release()
+{
+	for (size_t i = 0; i < m_components.size(); ++i)
+	{
+		BaseComponent* comp = m_components[i];
+		if (comp)
+		{
+			comp->Release();
+
+			delete comp;
+			comp = nullptr;
+		}
+	}
+}
+
+
+void BaseGameObject::AddComponent(BaseComponent* component)
+{
+	m_components.push_back(component);
+
+	sort(m_components.begin(), m_components.end(),
+		[](const BaseComponent* left, const BaseComponent* right) {
+			return left->GetOrder() < right->GetOrder();
+		});
+}
+
+void BaseGameObject::RemoveComponent(BaseComponent* component)
+{
+	remove(m_components.begin(), m_components.end(), component);
+}
+
+vector<BaseComponent*>& BaseGameObject::GetComponents()
+{
+	return m_components;
+}
---
+++ Object/BaseGameObject.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include "../Common.h"
+#include "../Util/Type/StatusType.h"
+#include "../Util/Type/LootType.h"
+#include "../Core/IBehavior.h"
+
+class BaseComponent;
+class BaseLevel;
+
+class BaseGameObject abstract : public IBehavior
+{
+public:
+	BaseGameObject(BaseLevel* level, const string& tag);
+	virtual ~BaseGameObject();
+
+private:
+	BaseLevel* m_level = nullptr;
+	string m_objectTag = " ";
+	vector<BaseComponent*> m_components;
+
+public:
+	virtual void Init() override;
+	virtual void Update() override;
+	virtual void Render() override;
+	virtual void Release() override;
+
+	void AddComponent(BaseComponent* component);
+	void RemoveComponent(BaseComponent* component);
+	vector<BaseComponent*>& GetComponents();
+
+	inline void SetTag(const string& tag) { m_objectTag = tag; }
+	inline string GetTag() { return m_objectTag; }
+	inline BaseLevel* GetLevel() { return m_level; }
+};
+

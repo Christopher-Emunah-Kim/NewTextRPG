64a07224d315f2a9517d54e8223c986e5e7c9d1e
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-20 17:16:22 +0900

    <feat> add GameInstance class for Player maintenance

---
+++ Core/GameInstance.cpp
@@ -0,0 +1,19 @@
+﻿#include "GameInstance.h"
+#include "../Data/PlayerDataTablePerLevel.h"
+
+
+void GameInstance::Init()
+{
+	m_player = Player();
+	m_bIsPlayerInitialzed = false;
+}
+
+bool GameInstance::IsPlayerInitialzed() const
+{
+	return m_bIsPlayerInitialzed;
+}
+
+void GameInstance::SetPlayerInitialize(bool bIsInitialized)
+{
+	m_bIsPlayerInitialzed = bIsInitialized;
+}
---
+++ Core/GameInstance.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "../Util/Singleton.h"
+#include "../Object/Player.h"
+
+
+class GameInstance : public Singleton<GameInstance>
+{
+	friend class Singleton<GameInstance>;
+	
+private:
+	GameInstance() = default;
+
+public:
+	virtual ~GameInstance() = default;
+
+private:
+	Player m_player;
+	bool m_bIsPlayerInitialzed = false;
+
+public:
+	void Init();
+	bool IsPlayerInitialzed() const;
+	void SetPlayerInitialize(bool bIsInitialized);
+
+	inline Player& GetPlayer() noexcept { return m_player; }
+};

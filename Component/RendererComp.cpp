a8bcfb332528f4da7861b82dd9bbf4d999c30033
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-20 14:33:40 +0900

    <feat> Add RendererComp and functions

---
+++ Component/RendererComp.cpp
@@ -0,0 +1,14 @@
+﻿#include "RendererComp.h"
+#include "../Object/BaseGameObject.h"
+#include "../Screen.h"
+
+
+RendererComp::RendererComp(BaseGameObject* owner, const string& renderShape)
+	: BaseComponent(owner), m_renderShape(renderShape)
+{
+}
+
+void RendererComp::Render()
+{
+	Screen::GetInstance()->Draw(m_owner->GetX(), m_owner->GetY(), m_renderShape);
+}
---
+++ Component/RendererComp.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "BaseComponent.h"
+
+class RendererComp : public BaseComponent
+{
+public:
+	RendererComp(BaseGameObject* owner, const string& renderShape);
+
+private:
+	string m_renderShape;
+
+public: 
+	virtual void Render() override;
+};
+

e72249aef41527288b781d7e951d7c94ef996fab
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-20 17:15:30 +0900

    <feat> add Title Level functions

--- Level/TestLevel.cpp
+++ Level/TestLevel.cpp
@@ -1,13 +1,27 @@
 ﻿#include "TestLevel.h"
 #include "../Object/BaseGameObject.h"
 #include "../Object/Player.h"
+#include "../Core/GameInstance.h"
 #include "../Util/InputSystem.h"
 
 
 void TestLevel::Init()
 {
-	m_gameObjects.push_back(new Player(this, "Player"));
-	
+	GameInstance* gameInstance = GameInstance::GetInstance();
+	Player& player = gameInstance->GetPlayer();
+
+	player.UpdateLevel(this);
+
+	if (false == gameInstance->IsPlayerInitialzed())
+	{
+		
+		gameInstance->SetPlayerInitialize(true);
+	}
+
+	if (FindObject("Player") == nullptr)
+	{
+		AddObject(&player);
+	}
 
 	BaseLevel::Init();
 }
--- Level/TitleLevel.cpp
+++ Level/TitleLevel.cpp
@@ -1,8 +1,24 @@
 ﻿#include "TitleLevel.h"
+#include "../Core/GameInstance.h"
 
 
 void TitleLevel::Init()
 {
+	GameInstance* gameInstance = GameInstance::GetInstance();
+	Player& player = gameInstance->GetPlayer();
+
+	player.UpdateLevel(this);
+
+	if (false == gameInstance->IsPlayerInitialzed())
+	{
+		gameInstance->SetPlayerInitialize(true);
+	}
+
+	if (FindObject("Player") == nullptr)
+	{
+		AddObject(&player);
+	}
+
 	BaseLevel::Init();
 }
 

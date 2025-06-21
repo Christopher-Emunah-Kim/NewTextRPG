81adb58c0c0d0817929baf27c7af014fd0a1dca1
Christopher <hashin.chris@gmail.com>
2025-06-21 09:35:36 +0900

    <feat> add SystemTextDialog class

---
+++ Object/SystemTextDialog.cpp
@@ -0,0 +1,36 @@
+﻿#include "SystemTextDialog.h"
+#include "../Component/SystemTextComp.h"
+
+SystemTextDialog::SystemTextDialog(BaseLevel* level)
+	: BaseGameObject(level, "SystemTextDialog")
+{ }
+
+
+void SystemTextDialog::Init()
+{
+	SetPosition(0, SCREEN_HEIGHT - 2);
+
+	if (IsComponentsEmpty())
+	{
+		AddComponent(new SystemTextComp(this));
+	}
+
+	BaseGameObject::Init();
+}
+
+void SystemTextDialog::SetSystemText(const string& text) noexcept
+{
+	SystemTextComp* textComp = nullptr;
+
+	vector<BaseComponent*> components = GetComponents();
+
+	for (size_t i = 0; i < components.size(); ++i)
+	{
+		textComp = dynamic_cast<SystemTextComp*>(components[i]);
+		if (textComp)
+		{
+			textComp->SetText(text);
+			break;
+		}
+	}
+}
---
+++ Object/SystemTextDialog.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "BaseGameObject.h"
+
+class SystemTextDialog : public BaseGameObject
+{
+public:
+	SystemTextDialog(BaseLevel* level);
+	virtual ~SystemTextDialog() = default;
+
+	virtual void Init() override;
+
+	void SetSystemText(const string& text) noexcept;
+};
+

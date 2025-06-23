bf4a30ca32826a96f758f019b266ff339bdf5726
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-23 21:49:54 +0900

    <fix> change OutputSystem

--- Util/OutputSystem.h
+++ Util/OutputSystem.h
@@ -12,19 +12,15 @@ public:
 		wcerr << L"[Error] " << errorMessage << endl;
 	}
 	static void PrintInfoMsg(const wstring& infoMessage) {
-		//wcout << L"[Info] " << infoMessage << endl;
 		GameInstance::GetInstance()->DisplaySystemText(L"[Info] " + infoMessage);
 	}
 	static void PrintWarningMsg(const wstring& warningMessage) {
-		//wcerr << L"[Warning] " << warningMessage << endl;
 		GameInstance::GetInstance()->DisplaySystemText(L"[Warning] " + warningMessage);
 	}
 	static void PrintSystemMsg(const wstring& systemMessage) {
-		//wcout << L"[System] " << systemMessage << endl;
 		GameInstance::GetInstance()->DisplaySystemText(L"[System] " + systemMessage);
 	}
 	static void PrintLine() {
-		//wcout << L"\n\n===========================================================\n\n" << endl;
 		GameInstance::GetInstance()->DisplaySystemText(L"\n\n===========================================================\n\n");
 	}
 	static void ClearScreen()
@@ -32,14 +28,4 @@ public:
 		system("cls");
 	}
 
-	/*static string ToLower(const string& str) {
-		string lowerStr = str;
-		transform(lowerStr.begin(), lowerStr.end(), lowerStr.begin(), ::tolower);
-		return lowerStr;
-	}
-	static string ToUpper(const string& str) {
-		string upperStr = str;
-		transform(upperStr.begin(), upperStr.end(), upperStr.begin(), ::toupper);
-		return upperStr;
-	}*/
 };

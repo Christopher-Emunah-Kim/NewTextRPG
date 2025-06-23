e001da668a021269c9a3a926b7bb32e36fc546f4
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-23 11:27:43 +0900

    <fix> rename PlayerInfoComp

---
+++ Component/Player/PlayerInfoComp.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "../BaseComponent.h"
+
+class Player;
+
+class PlayerStatusComp : public BaseComponent
+{
+public:
+	PlayerStatusComp(BaseGameObject* owner);
+	virtual ~PlayerStatusComp() = default;
+
+	virtual void Render(Screen* screen) override;
+	
+	void UpdateStatus(const Player& player);
+};
+

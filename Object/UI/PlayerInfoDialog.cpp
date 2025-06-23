b126dd3263f15d698ded865e3e3606c7442e735e
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-23 11:28:09 +0900

    <feat> add PlyaerInfoDialog class for rendering player info

---
+++ Object/UI/PlayerInfoDialog.cpp
@@ -0,0 +1,56 @@
+﻿#include "PlayerInfoDialog.h"
+#include "../../Level/BaseLevel.h"
+#include "../../Component/Player/PlayerInfoComp.h"
+
+PlayerInfoDialog::PlayerInfoDialog(BaseLevel* level)
+	: BaseGameObject(level, L"PlayerInfoDialog")
+{
+}
+
+void PlayerInfoDialog::Init()
+{
+	SetPosition(0, 0);
+
+	if (false == HasComponentType<PlayerInfoComp>())
+	{
+		AddComponent(new PlayerInfoComp(this));
+	}
+
+	BaseGameObject::Init();
+}
+
+void PlayerInfoDialog::UpdatePlayerInfoDialog(const Player& player)
+{
+	//TODO : 음 이걸 굳이 매번 새 객체포인터를 가지고 지정해야하는걸까?
+	PlayerInfoComp* infoComp = nullptr;
+	vector<BaseComponent*> components = GetComponents();
+
+	for (size_t i = 0; i < components.size(); ++i)
+	{
+		infoComp = dynamic_cast<PlayerInfoComp*>(components[i]);
+		if (infoComp)
+		{
+			infoComp->UpdateStatus(player);
+			break;
+		}
+	}
+
+}
+
+void PlayerInfoDialog::RegisterPlayerInfoDialogInNewLevel(BaseLevel* newLevel)
+{
+	if (GetLevel() != newLevel)
+	{
+		if (GetLevel())
+		{
+			GetLevel()->DetachObject(this);
+		}
+
+		SetLevel(newLevel);
+		
+		if (newLevel)
+		{
+			newLevel->AddObject(this);
+		}
+	}
+}
---
+++ Object/UI/PlayerInfoDialog.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "../BaseGameObject.h"
+
+class Player;
+class BaseLevel;
+class PlayerInfoComp;
+
+class PlayerInfoDialog : public BaseGameObject
+{
+public:
+	PlayerInfoDialog(BaseLevel* level);
+	virtual ~PlayerInfoDialog() = default;
+
+private:
+
+public:
+
+	virtual void Init() override;
+
+	void UpdatePlayerInfoDialog(const Player& player);
+	void RegisterPlayerInfoDialogInNewLevel(BaseLevel* newLevel);
+
+};
+

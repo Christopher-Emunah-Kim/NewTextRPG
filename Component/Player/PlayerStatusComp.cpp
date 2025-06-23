21139e8c654197bc4c78c5266f79a6d8259dc743
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-23 15:10:14 +0900

    <feat> add PlayerStatusComp and member functions

---
+++ Component/Player/PlayerStatusComp.cpp
@@ -0,0 +1,135 @@
+﻿#include "PlayerStatusComp.h"
+#include "../../Object/Player.h"
+
+PlayerStatusComp::PlayerStatusComp(BaseGameObject* owner)
+	:BaseComponent(owner),
+	m_playerName(owner->GetTag()),
+	m_level(1),
+	m_health(DEFAULT_CHARACTER_MAX_HEALTH),
+	m_maxHealth(DEFAULT_CHARACTER_MAX_HEALTH),
+	m_experience(0),
+	m_maxExperience(DEFAULT_PLAYER_MAX_EXPERIENCE),
+	m_gold(0),
+	m_status(Status(DEFAULT_ATTACK, DEFAULT_DEFENSE, DEFAULT_AGILITY))
+{
+	LoadStatusByLevel();
+}
+
+bool PlayerStatusComp::GainExperience(const int32& exp)
+{
+	m_experience += exp;
+
+	return CheckLevelUp();
+}
+
+bool PlayerStatusComp::CheckLevelUp()
+{
+	bool bIsLeveledUp = false;
+
+	while (m_experience >= m_maxExperience)
+	{
+		m_level++;
+		m_experience -= m_maxExperience;
+		bIsLeveledUp = true;
+		LoadStatusByLevel();
+	}
+
+	return bIsLeveledUp;
+}
+
+void PlayerStatusComp::LoadStatusByLevel()
+{
+	PlayerDataTablePerLevel dataTable;
+
+	FLevelProperties levelProps = dataTable.LoadLevelData(m_level);
+
+	m_maxExperience = levelProps.maxExperiencePerLevel;
+	m_maxHealth = levelProps.maxHealthPerLevel;
+
+	m_status = Status(
+		levelProps.attackPerLevel,
+		levelProps.defensePerLevel,
+		levelProps.agilityPerLevel
+	);
+}
+
+void PlayerStatusComp::SetStatus(const Status& status)
+{
+	m_status = status;
+}
+
+void PlayerStatusComp::SetPlayerLevel(const int16& level)
+{
+	m_level = level;
+
+	if (m_level < 1)
+	{
+		m_level = 1;
+	}
+	if (m_level > 100)
+	{
+		m_level = 100;
+	}
+
+	LoadStatusByLevel();
+}
+
+void PlayerStatusComp::SetPlayerHealth(const int32& health)
+{
+	m_health = health;
+	if (m_health < 0)
+	{
+		m_health = 0;
+	}
+	if (m_health > m_maxHealth)
+	{
+		m_health = m_maxHealth;
+	}
+}
+
+void PlayerStatusComp::SetPlayerMaxHealth(const int32& maxHealth)
+{
+	m_maxHealth = maxHealth;
+
+	if (m_maxHealth < 1)
+	{
+		m_maxHealth = 1;
+	}
+}
+
+void PlayerStatusComp::SetPlayerExperience(const int32& exp)
+{
+	m_experience = exp;
+
+	if (m_experience < 0)
+	{
+		m_experience = 0;
+	}
+
+	CheckLevelUp();
+}
+
+void PlayerStatusComp::SetPlayerMaxExperience(const int32& maxExp)
+{
+	m_maxExperience = maxExp;
+
+	if (m_maxExperience < 1)
+	{
+		m_maxExperience = 1;
+	}
+}
+
+void PlayerStatusComp::SetPlayerGold(const int32& gold)
+{
+	m_gold = gold;
+
+	if (m_gold < 0)
+	{
+		m_gold = 0;
+	}
+}
+
+void PlayerStatusComp::SetPlayerName(const wstring& name)
+{
+	m_playerName = name;
+}
---
+++ Component/Player/PlayerStatusComp.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include "../BaseComponent.h"
+#include "../../Util/Type/StatusType.h"
+#include "../../Util/Type/LootType.h"
+
+
+class PlayerStatusComp : public BaseComponent
+{
+public:
+	PlayerStatusComp(BaseGameObject* owner);
+	virtual ~PlayerStatusComp() = default;
+
+private:
+	Status		m_status;
+	wstring		m_playerName;
+	int16		m_level;
+	int32		m_health;
+	int32		m_maxHealth;
+	int32		m_experience;
+	int32		m_maxExperience;
+	int32		m_gold;
+
+public:
+	bool GainExperience(const int32& exp);
+	bool CheckLevelUp();
+	void LoadStatusByLevel();
+
+	void SetStatus(const Status& status);
+	void SetPlayerLevel(const int16& level);
+	void SetPlayerHealth(const int32& health);
+	void SetPlayerMaxHealth(const int32& maxHealth);
+	void SetPlayerExperience(const int32& exp);
+	void SetPlayerMaxExperience(const int32& maxExp);
+	void SetPlayerGold(const int32& gold);
+	void SetPlayerName(const wstring& name);
+
+	const Status& GetStatus() const { return m_status; }
+	const int16& GetPlayerLevel() const { return m_level; }
+	const int32& GetPlayerHealth() const { return m_health; }
+	const int32& GetPlayerMaxHealth() const { return m_maxHealth; }
+	const int32& GetPlayerExperience() const { return m_experience; }
+	const int32& GetPlayerMaxExperience() const { return m_maxExperience; }
+	const int32& GetPlayerGold() const { return m_gold; }
+	const wstring& GetPlayerName() const { return m_playerName; }
+};
+

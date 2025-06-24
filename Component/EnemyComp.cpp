9ff7147c5c2696fda557f710f9e397c68f770a19
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-24 20:15:36 +0900

    <feat> add EnemyComp for replacing Enemy objects

---
+++ Component/EnemyComp.cpp
@@ -0,0 +1,89 @@
+﻿#include "EnemyComp.h"
+#include "../Core/GameInstance.h"
+#include "../Object/BaseGameObject.h"
+#include "Player/PlayerStatusComp.h"
+
+
+EnemyComp::EnemyComp(BaseGameObject* owner)
+	:BaseComponent(owner, 4),
+	m_health(30),
+	m_maxHealth(30),
+	m_status(8,3,5),
+	m_dropGold(10),
+	m_dropExp(15),
+	m_dropItem(L"None")
+{
+
+}
+
+void EnemyComp::Init()
+{
+	m_owner = GetOwner();
+
+	if (m_owner->HasCustomField(L"health"))
+	{
+		m_health = stoi(m_owner->GetCustomField(L"health"));
+	}
+
+	if (m_owner->HasCustomField(L"maxHealth"))
+	{
+		m_maxHealth = stoi(m_owner->GetCustomField(L"maxHealth"));
+	}
+
+	if (m_owner->HasCustomField(L"attack") && m_owner->HasCustomField(L"defense") && m_owner->HasCustomField(L"agility"))
+	{
+		int16 atk = stoi(m_owner->GetCustomField(L"attack"));
+		int16 def = stoi(m_owner->GetCustomField(L"defense"));
+		int16 agi = stoi(m_owner->GetCustomField(L"agility"));
+		m_status = Status(atk, def, agi);
+	}
+
+	if (m_owner->HasCustomField(L"dropExp"))
+	{
+		m_dropExp = stoi(m_owner->GetCustomField(L"dropExp"));
+	}
+
+	if (m_owner->HasCustomField(L"dropItem"))
+	{
+		m_dropItem = m_owner->GetCustomField(L"dropItem");
+	}
+}
+
+void EnemyComp::LoadStateFromString(const wstring& state)
+{
+	m_owner->LoadStateFromString(state);
+}
+
+bool EnemyComp::TakeDamage(int32 damage)
+{
+	m_health -= damage;
+	if (m_health <= 0)
+	{
+		m_health = 0;
+		return true;
+	}
+	return false;
+}
+
+void EnemyComp::ProcessDrop(BaseGameObject* player)
+{
+	if (nullptr == player)
+	{
+		return;
+	}
+
+	PlayerStatusComp* statusComp = player->GetComponentsByType<PlayerStatusComp>();
+	if (statusComp)
+	{
+		statusComp->GainExperience(m_dropExp);
+		statusComp->GainPlayerGold(statusComp->GetPlayerGold());
+
+		GameInstance::GetInstance()->DisplaySystemText(L"경험치 " + to_wstring(m_dropExp) + L"와 골드 " +
+			to_wstring(m_dropGold) + L"를 획득했습니다.");
+
+		if (m_dropItem != L"None")
+		{
+			GameInstance::GetInstance()->DisplaySystemText(L"아이템 " + m_dropItem + L"을(를) 획득했습니다.");
+		}
+	}
+}
---
+++ Component/EnemyComp.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include "BaseComponent.h"
+
+class BaseGameObject;
+
+class EnemyComp : public BaseComponent
+{
+public:
+	EnemyComp(BaseGameObject* owner);
+	virtual ~EnemyComp() = default;
+
+private:
+	int32 m_health;
+	int32 m_maxHealth;
+	Status m_status;
+	int32 m_dropGold;
+	int32 m_dropExp;
+	wstring m_dropItem;
+
+public:
+	virtual void Init() override;
+	
+	void LoadStateFromString(const wstring& state);
+
+	bool TakeDamage(int32 damage);
+	void ProcessDrop(BaseGameObject* player);
+
+
+	inline void SetHealth(const int32& health) { m_health = health; }
+	inline void SetMaxHealth(const int32& maxHealth) { m_maxHealth = maxHealth; }
+
+};
+

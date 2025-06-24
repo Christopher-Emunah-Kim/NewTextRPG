a76bf5186c9794bb591c1a67d357802f36cff78e
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-24 20:14:47 +0900

    <docs> rename RendererComp

---
+++ Component/SystemRendererComp.cpp
@@ -0,0 +1,53 @@
+﻿#include "SystemRendererComp.h"
+#include "../Object/BaseGameObject.h"
+#include "../Screen.h"
+
+
+SystemRendererComp::SystemRendererComp(BaseGameObject* owner)
+	: BaseComponent(owner)
+{
+}
+
+void SystemRendererComp::Render(Screen* screen)
+{
+}
+
+void SystemRendererComp::Update()
+{
+
+}
+
+void SystemRendererComp::SetText(const wstring& text)
+{
+
+}
+
+void SystemRendererComp::EnqueueText(const wstring& text)
+{
+
+}
+
+void SystemRendererComp::ClearTexts()
+{
+
+}
+
+void SystemRendererComp::AddOption(const wstring& option, const wstring& eventName)
+{
+}
+
+void SystemRendererComp::ClearOption()
+{
+}
+
+void SystemRendererComp::SelectNextOption()
+{
+}
+
+void SystemRendererComp::SelectPreviousOption()
+{
+}
+
+void SystemRendererComp::ConfirmSelection()
+{
+}
---
+++ Component/SystemRendererComp.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include "BaseComponent.h"
+#include "../Common.h"
+
+
+class SystemRendererComp : public BaseComponent
+{
+public:
+	SystemRendererComp(BaseGameObject* owner);
+	virtual ~SystemRendererComp() = default;
+
+private:
+	queue<wstring> m_textQueue;
+	int8 m_selectedOption = 0;
+	vector<wstring> m_optionTexts;
+	vector<wstring> m_optionEvents;
+	bool m_IsAwaitingSlections = false;
+
+public: 
+	virtual void Render(Screen* screen) override;
+	virtual void Update() override;
+
+	void SetText(const wstring& text);
+	void EnqueueText(const wstring& text);
+	void ClearTexts();
+
+	void AddOption(const wstring& option, const wstring& eventName);
+	void ClearOption();
+	void SelectNextOption();
+	void SelectPreviousOption();
+	void ConfirmSelection();
+
+	inline bool HasOptions() const { return !m_optionTexts.empty(); }
+	inline int8 GetSelectedOption() const noexcept { return m_selectedOption; }
+	inline const vector<wstring>& GetOptionTexts() const { return m_optionTexts; }
+};
+

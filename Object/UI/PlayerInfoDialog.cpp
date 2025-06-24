4c42ff6bbd9baf5ab2667b5b307b0fd6cf54afde
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-24 20:21:25 +0900

    <docs> remove PlayerInfoDialog

--- Object/UI/PlayerInfoDialog.cpp
+++ Object/UI/PlayerInfoDialog.cpp
@@ -1,106 +1,106 @@
-﻿#include "PlayerInfoDialog.h"
-#include "../../Level/BaseLevel.h"
-#include "../../Component/Player/PlayerInfoRenderComp.h"
-#include "../../Object/Player.h"
-#include "../../Util/OutputSystem.h"
-#include "../../Component/Player/PlayerStatusComp.h"
-
-PlayerInfoDialog::PlayerInfoDialog(BaseLevel* level)
-	: BaseGameObject(level, L"PlayerInfoDialog")
-{
-}
-
-void PlayerInfoDialog::Init()
-{
-	SetPosition(0, 0);
-
-	//OutputSystem::PrintInfoMsg(L"PlayerInfoDialog::Init 호출됨");
-
-	if (false == HasComponentType<PlayerInfoRenderComp>())
-	{
-		m_renderComp =  new PlayerInfoRenderComp(this);
-		AddComponent(m_renderComp);
-		//OutputSystem::PrintInfoMsg(L"PlayerInfoRenderComp 생성됨");
-	}
-	else
-	{
-		vector<BaseComponent*> comps = GetComponents();
-		for (size_t i = 0; i < comps.size(); ++i)
-		{
-			m_renderComp = dynamic_cast<PlayerInfoRenderComp*>(comps[i]);
-			if (m_renderComp)
-			{
-				//OutputSystem::PrintInfoMsg(L"기존 PlayerInfoRenderComp 찾음");
-				break;
-			}
-		}
-	}
-
-	BaseGameObject::Init();
-}
-
-void PlayerInfoDialog::UpdatePlayerInfoDialog(const Player& player)
-{
-	//TODO : 음 이걸 굳이 매번 새 객체포인터를 가지고 지정해야하는걸까?
-	//TODO : 수정필요
-	const PlayerStatusComp* statusComp = nullptr;
-    const vector<BaseComponent*>& playerComps = const_cast<Player&>(player).GetComponents();
-
-	for (size_t i = 0; i < playerComps.size(); ++i)
-	{
-		statusComp = dynamic_cast<PlayerStatusComp*>(playerComps[i]);
-		if (statusComp)
-		{
-			break;
-		}
-	}
-
-	if (nullptr == statusComp)
-	{
-		OutputSystem::PrintErrorMsg(L"플레이어에 PlayerStatusComp가 없습니다.");
-		return;
-	}
-
-
-	if (m_renderComp)
-	{
-		const Status& status = statusComp->GetStatus();
-
-		m_renderComp->UpdateStatus(
-			statusComp->GetPlayerName(),
-			statusComp->GetPlayerLevel(),
-			statusComp->GetPlayerHealth(),
-			statusComp->GetPlayerMaxHealth(),
-			statusComp->GetPlayerExperience(),
-			statusComp->GetPlayerMaxExperience(),
-			status.GetAttack(),
-			status.GetDefense(),
-			status.GetAgility(),
-			statusComp->GetPlayerGold()
-		);
-	}
-	else
-	{
-		OutputSystem::PrintErrorMsg(L"PlayerInfoDialog에 PlayerInfoRenderComp가 존재하지 않습니다.");
-	}
-	
-
-}
-
-void PlayerInfoDialog::RegisterPlayerInfoDialogInNewLevel(BaseLevel* newLevel)
-{
-	if (GetLevel() != newLevel)
-	{
-		if (GetLevel())
-		{
-			GetLevel()->DetachObject(this);
-		}
-
-		SetLevel(newLevel);
-		
-		if (newLevel)
-		{
-			newLevel->AddObject(this);
-		}
-	}
-}
+﻿//#include "PlayerInfoDialog.h"
+//#include "../../Level/BaseLevel.h"
+//#include "../../Component/Player/PlayerInfoRenderComp.h"
+//#include "../../Object/Player.h"
+//#include "../../Util/OutputSystem.h"
+//#include "../../Component/Player/PlayerStatusComp.h"
+//
+//PlayerInfoDialog::PlayerInfoDialog(BaseLevel* level)
+//	: BaseGameObject(level, L"PlayerInfoDialog")
+//{
+//}
+//
+//void PlayerInfoDialog::Init()
+//{
+//	SetPosition(0, 0);
+//
+//	//OutputSystem::PrintInfoMsg(L"PlayerInfoDialog::Init 호출됨");
+//
+//	if (false == HasComponentType<PlayerInfoRenderComp>())
+//	{
+//		m_renderComp =  new PlayerInfoRenderComp(this);
+//		AddComponent(m_renderComp);
+//		//OutputSystem::PrintInfoMsg(L"PlayerInfoRenderComp 생성됨");
+//	}
+//	else
+//	{
+//		vector<BaseComponent*> comps = GetComponents();
+//		for (size_t i = 0; i < comps.size(); ++i)
+//		{
+//			m_renderComp = dynamic_cast<PlayerInfoRenderComp*>(comps[i]);
+//			if (m_renderComp)
+//			{
+//				//OutputSystem::PrintInfoMsg(L"기존 PlayerInfoRenderComp 찾음");
+//				break;
+//			}
+//		}
+//	}
+//
+//	BaseGameObject::Init();
+//}
+//
+//void PlayerInfoDialog::UpdatePlayerInfoDialog(const Player& player)
+//{
+//	//TODO : 음 이걸 굳이 매번 새 객체포인터를 가지고 지정해야하는걸까?
+//	//TODO : 수정필요
+//	const PlayerStatusComp* statusComp = nullptr;
+//    const vector<BaseComponent*>& playerComps = const_cast<Player&>(player).GetComponents();
+//
+//	for (size_t i = 0; i < playerComps.size(); ++i)
+//	{
+//		statusComp = dynamic_cast<PlayerStatusComp*>(playerComps[i]);
+//		if (statusComp)
+//		{
+//			break;
+//		}
+//	}
+//
+//	if (nullptr == statusComp)
+//	{
+//		OutputSystem::PrintErrorMsg(L"플레이어에 PlayerStatusComp가 없습니다.");
+//		return;
+//	}
+//
+//
+//	if (m_renderComp)
+//	{
+//		const Status& status = statusComp->GetStatus();
+//
+//		m_renderComp->UpdateStatus(
+//			statusComp->GetPlayerName(),
+//			statusComp->GetPlayerLevel(),
+//			statusComp->GetPlayerHealth(),
+//			statusComp->GetPlayerMaxHealth(),
+//			statusComp->GetPlayerExperience(),
+//			statusComp->GetPlayerMaxExperience(),
+//			status.GetAttack(),
+//			status.GetDefense(),
+//			status.GetAgility(),
+//			statusComp->GetPlayerGold()
+//		);
+//	}
+//	else
+//	{
+//		OutputSystem::PrintErrorMsg(L"PlayerInfoDialog에 PlayerInfoRenderComp가 존재하지 않습니다.");
+//	}
+//	
+//
+//}
+//
+//void PlayerInfoDialog::RegisterPlayerInfoDialogInNewLevel(BaseLevel* newLevel)
+//{
+//	if (GetLevel() != newLevel)
+//	{
+//		if (GetLevel())
+//		{
+//			GetLevel()->DetachObject(this);
+//		}
+//
+//		SetLevel(newLevel);
+//		
+//		if (newLevel)
+//		{
+//			newLevel->AddObject(this);
+//		}
+//	}
+//}
--- Object/UI/PlayerInfoDialog.h
+++ Object/UI/PlayerInfoDialog.h
@@ -1,25 +1,25 @@
-﻿#pragma once
-#include "../BaseGameObject.h"
-
-class Player;
-class BaseLevel;
-class PlayerInfoRenderComp;
-
-class PlayerInfoDialog final : public BaseGameObject
-{
-public:
-	PlayerInfoDialog(BaseLevel* level);
-	virtual ~PlayerInfoDialog() = default;
-
-private:
-	PlayerInfoRenderComp* m_renderComp = nullptr;
-
-public:
-
-	virtual void Init() override;
-
-	void UpdatePlayerInfoDialog(const Player& player);
-	void RegisterPlayerInfoDialogInNewLevel(BaseLevel* newLevel);
-
-};
-
+﻿//#pragma once
+//#include "../BaseGameObject.h"
+//
+//class Player;
+//class BaseLevel;
+//class PlayerInfoRenderComp;
+//
+//class PlayerInfoDialog final : public BaseGameObject
+//{
+//public:
+//	PlayerInfoDialog(BaseLevel* level);
+//	virtual ~PlayerInfoDialog() = default;
+//
+//private:
+//	PlayerInfoRenderComp* m_renderComp = nullptr;
+//
+//public:
+//
+//	virtual void Init() override;
+//
+//	void UpdatePlayerInfoDialog(const Player& player);
+//	void RegisterPlayerInfoDialogInNewLevel(BaseLevel* newLevel);
+//
+//};
+//

55bd757cde687acce49195b1e53c206a85ad4047
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-27 11:04:19 +0900

    <feat> add BattleComponent for BattleCharacter class

---
+++ Component/BattleComponent.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "BaseComponent.h"
+#include "../Object/Character/BattleCharacter.h"
+
+
+class BattleComponent final : public BaseComponent
+{
+public:
+	BattleComponent(BaseGameObject* owner, int32 order = 0);
+	virtual ~BattleComponent();
+
+private:
+	BattleCharacter* m_owner;
+	BattleCharacter* m_currentTarget;
+	bool m_bIsBattleActive;
+	bool m_bIsPlayerTurn;
+
+public:
+	void InitiateBattle(BattleCharacter* target);
+	void EndBattle();
+	void ProcessBattleTurn();
+
+	bool DetermineFirstAttacker();
+	void ExecuteAttack();
+	bool CheckBattleEnd();
+	void HandleBattleRewards();
+
+	bool IsBattleActive() const noexcept { return m_bIsBattleActive; }
+	bool IsPlayerTurn() const noexcept { return m_bIsPlayerTurn; }
+	BattleCharacter* GetCurrentTarget() const { return m_currentTarget; }
+};
+

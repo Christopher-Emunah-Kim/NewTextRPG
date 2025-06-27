e3aa3813afe0a520be851f4c31775b5bd58670ea
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-27 11:03:58 +0900

    <feat> add BattleCharacter and functions

---
+++ Object/Character/BattleCharacter.cpp
@@ -0,0 +1,108 @@
+﻿#include "BattleCharacter.h"
+#include "../../Core/GameInstance.h"
+#include "../../Component/BattleComponent.h"
+
+
+BattleCharacter::BattleCharacter(BaseLevel* level, const wstring& tag)
+	: BaseCharacter(level, tag), m_battleCharacterInfo(DEFAULT_LEVEL)
+{
+}
+
+BattleCharacter::~BattleCharacter()
+{
+}
+
+void BattleCharacter::Init()
+{
+	BaseCharacter::Init();
+
+	if (!HasComponentType<BattleComponent>())
+	{
+		AddComponent(new BattleComponent(this));
+	}
+}
+
+void BattleCharacter::Interact(BaseCharacter* other)
+{
+	BattleCharacter* battleTarget = dynamic_cast<BattleCharacter*>(other);
+	if (battleTarget && IsAlive() && battleTarget->IsAlive())
+	{
+		BattleComponent* battleComp = GetComponentsByType<BattleComponent>();
+		if (battleComp)
+		{
+			battleComp->InitiateBattle(battleTarget);
+		}
+		else
+		{
+			Attack(battleTarget);
+		}
+	}
+}
+
+void BattleCharacter::Attack(BattleCharacter* target)
+{
+	if (nullptr == target)
+	{
+		return;
+	}
+
+	int32 clculatedDamage = CalculateDamage(target);
+	GameInstance::GetInstance()->DisplaySystemText(GetName() + L"가(이) " + target->GetName() + L" 을(를) 공격합니다!");
+
+	target->TakeDamage(clculatedDamage);
+}
+
+void BattleCharacter::TakeDamage(const int32& damage)
+{
+	if (damage <= 0)
+	{
+		return;
+	}
+
+	m_battleCharacterInfo.health -= damage;
+
+	if (m_battleCharacterInfo.health < 0)
+	{
+		m_battleCharacterInfo.health = 0;
+	}
+
+	GameInstance::GetInstance()->DisplaySystemText(GetName() + L"가(이) " + to_wstring(damage) + L" 의 피해를 입었습니다.");
+}
+
+bool BattleCharacter::IsAlive() const
+{
+	return m_battleCharacterInfo.health > 0;
+}
+
+int32 BattleCharacter::CalculateDamage(BattleCharacter* target) const
+{
+	int32 damage = m_battleCharacterInfo.status.GetAttack() - target->GetStatus().GetDefense();
+
+	if (damage > 0)
+	{
+		return damage;
+	}
+	else
+	{
+		return DEFAULT_DAMAGE;
+	}
+}
+
+void BattleCharacter::SetHealth(const int32& health)
+{
+	m_battleCharacterInfo.health = health;
+	if (m_battleCharacterInfo.health > m_battleCharacterInfo.maxHealth)
+	{
+		m_battleCharacterInfo.health = m_battleCharacterInfo.maxHealth;
+	}
+}
+
+void BattleCharacter::SetMaxHealth(const int32& maxHealth)
+{
+	m_battleCharacterInfo.maxHealth = maxHealth;
+}
+
+void BattleCharacter::SetStatus(const Status& status)
+{
+	m_battleCharacterInfo.status = status;
+}
---
+++ Object/Character/BattleCharacter.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include "BaseCharacter.h"
+#include "../../Util/Type/CharacterInfo.h"
+
+constexpr __int32 DEFAULT_DAMAGE = 3;
+
+class BattleCharacter  abstract : public BaseCharacter
+{
+public:
+	BattleCharacter(BaseLevel* level, const wstring& tag);
+	virtual ~BattleCharacter();
+
+protected:
+	FBattleCharacterInfo m_battleCharacterInfo;
+
+public:
+	virtual void Init() override;
+	virtual void Interact(BaseCharacter* other) override;
+
+	virtual void Attack(BattleCharacter* target);
+	virtual void TakeDamage(const int32& damage);
+	virtual bool IsAlive() const;
+
+	int32 CalculateDamage(BattleCharacter* target) const;
+
+	void SetHealth(const int32& health);
+	void SetMaxHealth(const int32& maxHealth);
+	void SetStatus(const Status& status);
+
+	int32 GetHealth() const noexcept { return m_battleCharacterInfo.health; }
+	int32 GetMaxHealth() const noexcept { return m_battleCharacterInfo.maxHealth; }
+	const Status& GetStatus() const noexcept { return m_battleCharacterInfo.status; }
+	const FBattleCharacterInfo& GetBattleCharacterInfo() const noexcept { return m_battleCharacterInfo; }
+};
+

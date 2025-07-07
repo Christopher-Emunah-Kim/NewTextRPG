0439f150080945310cdb043bc9babaedfe591ac4
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-07-07 12:26:41 +0900

    <feat> Add InventoryItem

---
+++ Item/InventoryItem.cpp
@@ -0,0 +1,26 @@
+﻿#include "InventoryItem.h"
+
+InventoryItem::InventoryItem(int64 serialNumber, int32 itemId)
+: m_serialNumber(serialNumber), m_itemId(itemId)
+{
+}
+
+int64 InventoryItem::CreateSerialNumber()
+{
+	const time_point now = system_clock::now();
+	const int64 timestamp = duration_cast<milliseconds>(now.time_since_epoch()).count();
+
+	srand(static_cast<unsigned int>(time(NULL)));
+	const int32 randomNumber = rand() % 1000000; 
+
+	stringstream ss;
+	ss << timestamp << setfill('0') << setw(6) << randomNumber;
+	return stoll(ss.str());
+}
+
+InventoryItem InventoryItem::Create(int32 itemId)
+{
+	const int64 serialNumber = CreateSerialNumber();
+
+	return InventoryItem(serialNumber, itemId);
+}
---
+++ Item/InventoryItem.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include "../Common.h"
+
+class InventoryItem
+{
+private:
+	InventoryItem(int64 serialNumber, int32 itemId);
+
+public:
+	virtual ~InventoryItem() = default;
+
+	InventoryItem(const InventoryItem&) = delete;
+	InventoryItem& operator=(const InventoryItem&) = delete;
+
+	InventoryItem(InventoryItem&&) = default;
+	InventoryItem& operator=(InventoryItem&&) = default;	
+
+
+private:
+	static int64 CreateSerialNumber();
+
+public:
+	static InventoryItem  Create(int32 itemId);
+
+	int64 GetSerialNumber() const { return m_serialNumber; }
+	int32 GetItemId() const { return m_itemId; }
+
+
+private:
+	const int64 m_serialNumber;
+	const int32 m_itemId;
+
+};
+
